c75205fe5421ce40a1f412ee1bbc6ed33183f444
huanruiz <huanruizhang@trip.com>
2023-07-03 10:12:53 +0800

    aop

--- chapter6/src/main/java/com/smart/event/MailSendEvent.java
+++ chapter6/src/main/java/com/smart/event/MailSendEvent.java
@@ -4,14 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ApplicationContextEvent;
 
 public class MailSendEvent extends ApplicationContextEvent {
+
+	/**
+	 * 发送地
+	 */
 	private String to;
 	
 	public MailSendEvent(ApplicationContext source, String to) {
 		super(source);
 		this.to = to;
 	}
+
 	public String getTo() {
-		
 		return this.to;
 	}
 }
--- chapter6/src/main/java/com/smart/event/MailSender.java
+++ chapter6/src/main/java/com/smart/event/MailSender.java
@@ -6,16 +6,16 @@ import org.springframework.context.ApplicationContextAware;
 
 public class MailSender implements ApplicationContextAware {
 
-	private ApplicationContext ctx ;
+    private ApplicationContext ctx;
 
-	public void setApplicationContext(ApplicationContext ctx)
-			throws BeansException {
-		this.ctx = ctx;
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.ctx = ctx;
+    }
 
-	}	
-	public void sendMail(String to){
-		System.out.println("MailSender:模拟发送邮件...");
-		MailSendEvent mse = new MailSendEvent(this.ctx,to);
-		ctx.publishEvent(mse);
-	}
+    public void sendMail(String to) {
+        System.out.println("MailSender:模拟发送邮件...");
+        MailSendEvent mse = new MailSendEvent(this.ctx, to);
+        ctx.publishEvent(mse);
+    }
 }
