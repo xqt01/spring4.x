8014b20b36c749796d9de2021c0281a9aaaaa5e4
huanruiz <huanruizhang@trip.com>
2023-07-11 10:24:19 +0800

    update

--- chapter7/src/main/java/com/smart/advice/GreetingBeforeAdvice.java
+++ chapter7/src/main/java/com/smart/advice/GreetingBeforeAdvice.java
@@ -5,6 +5,10 @@ import java.lang.reflect.Method;
 import org.springframework.aop.MethodBeforeAdvice;
 
 public class GreetingBeforeAdvice implements MethodBeforeAdvice {
+
+	/**
+	 * 在目标类方法调用前执行
+	 */
 	public void before(Method method, Object[] args, Object obj) throws Throwable {
 		String clientName = (String)args[0];
 		System.out.println("How are you！Mr."+clientName+".");
--- chapter7/src/test/java/com/smart/advice/BeforeAdviceTest.java
+++ chapter7/src/test/java/com/smart/advice/BeforeAdviceTest.java
@@ -2,15 +2,16 @@ package com.smart.advice;
 
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.framework.ProxyFactory;
-import static org.testng.Assert.*;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 public class BeforeAdviceTest {
 
     @Test
 	public void before() {
         Waiter target = new NaiveWaiter();
-        BeforeAdvice  advice = new GreetingBeforeAdvice();
+        BeforeAdvice advice = new GreetingBeforeAdvice();
+        // 代理接口使用JdkDynamicAopProxy, 代理类使用Cglib2AopProxy. 开启优化后, 对接口代理也可能使用Cglib2AopProxy
+        // 在使用CGLib动态代理技术时, 要引入CGLib类库
         ProxyFactory pf = new ProxyFactory();
         pf.setInterfaces(target.getClass().getInterfaces());
         pf.setOptimize(true);
