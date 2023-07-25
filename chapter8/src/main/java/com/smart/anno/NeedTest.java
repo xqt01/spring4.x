24f3322e1a36a1b69309801179c04fcb1fff488a
huanruiz <huanruizhang@trip.com>
2023-07-25 09:56:46 +0800

    anno

--- chapter8/src/main/java/com/smart/anno/NeedTest.java
+++ chapter8/src/main/java/com/smart/anno/NeedTest.java
@@ -5,9 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface NeedTest{
-	boolean value() default false;
+public @interface NeedTest {
+    boolean value() default false;
 }
