91243663c4825fc7ce6d4cef62ebfb4327d644e8
huanruiz <huanruizhang@trip.com>
2023-08-08 10:42:14 +0800

    夹缝中学习...

--- chapter11/src/main/java/com/smart/service/ForumService1.java
+++ chapter11/src/main/java/com/smart/service/ForumService1.java
@@ -1,35 +1,37 @@
 package com.smart.service;
 
+import com.smart.dao.ForumDao;
+import com.smart.domain.Forum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
-import com.smart.dao.ForumDao;
-import com.smart.domain.Forum;
+
+/**
+ * 手动设置编程式的事务管理
+ */
 @Service
 public class ForumService1 {
-	public ForumDao forumDao;
-	public TransactionTemplate template;
-	public void addForum(final Forum forum) {
-		
-		template.execute(new TransactionCallbackWithoutResult() {
+    public ForumDao forumDao;
+    public TransactionTemplate template;
+
+    public void addForum(final Forum forum) {
+        template.execute(new TransactionCallbackWithoutResult() {
+            @Override
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
-				forumDao.addForum(forum);
-			}
-		});
+                forumDao.addForum(forum);
+            }
+        });
+    }
 
-	}
+    @Autowired
+    public void setForumDao(ForumDao forumDao) {
+        this.forumDao = forumDao;
+    }
 
-	@Autowired
-	public void setForumDao(ForumDao forumDao) {
-		this.forumDao = forumDao;
-	}
-	@Autowired
-	public void setTemplate(TransactionTemplate template) {
-		this.template = template;
-	}
-
-	
-	
+    @Autowired
+    public void setTemplate(TransactionTemplate template) {
+        this.template = template;
+    }
 }
